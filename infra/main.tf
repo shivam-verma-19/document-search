36519f4c79cb2895551c46271a106118e273eff4
shivam-verma-19 <sourav.shivam1998@gmail.com>
2026-05-05 01:41:47 +0530

    removed duplicate dynamodb

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 
 # S3 bucket for Terraform state
@@ -24,22 +24,5 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
-  }
-}
-
-# DynamoDB for locking
-resource "aws_dynamodb_table" "tf_lock" {
-  name         = "rag-terraform-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  ttl {
-    attribute_name = "ttl"
-    enabled        = true
-  } 
-
-  attribute {
-    name = "LockID"
-    type = "S"
   }
 }
