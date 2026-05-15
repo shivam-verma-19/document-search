24cf3d3d9918b62cc92ef3d1ebbcb053a4ad7a10
shivam-verma-19 <sourav.shivam1998@gmail.com>
2026-05-15 23:07:21 +0530

    visibility and batch size added

--- infra/sqs.tf
+++ infra/sqs.tf
@@ -1,6 +1,8 @@
 resource "aws_sqs_queue" "rag_queue" {
   name = "rag-processing-queue"
 
+  visibility_timeout_seconds = 180
+
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.dlq.arn
     maxReceiveCount     = 5
@@ -14,4 +16,6 @@ resource "aws_sqs_queue" "dlq" {
 resource "aws_lambda_event_source_mapping" "sqs_trigger" {
   event_source_arn = aws_sqs_queue.rag_queue.arn
   function_name    = aws_lambda_function.rag_ingest_worker.arn
+
+  batch_size = 1
 }
