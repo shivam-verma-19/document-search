185f1eaf25b673665b7b4bcecd26fd20666f11b0
shivam-verma-19 <sourav.shivam1998@gmail.com>
2026-05-15 15:48:55 +0530

    lint f

--- backend/app/reranker.py
+++ backend/app/reranker.py
@@ -10,7 +10,7 @@ _bedrock_client = None
 def _get_client():
     global _bedrock_client
     if _bedrock_client is None:
-        _bedrock_client = boto3.client("bedrock-runtime", region_name=AWS_REGION) #type: ignore
+        _bedrock_client = boto3.client("bedrock-runtime", region_name=AWS_REGION)  # type: ignore
     return _bedrock_client
 
 
